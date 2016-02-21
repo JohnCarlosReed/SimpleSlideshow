a964ad3b144f53d8555ca061c5f73f1b06104cff
John Reed <johnnycarlos@gmail.com>
2016-02-20 20:08:19 -0800

    added comments

--- simpleslideshow.py
+++ simpleslideshow.py
@@ -1,3 +1,9 @@
+"""
+This is a barebones slideshow application using OpenCV as an experiment and excuse
+to learn something about OpenCV.
+The image_path, windowHeight, windowWidth are hard-coded and need to be set manually.
+"""
+
 import numpy as np
 import cv2
 import os
@@ -12,6 +18,10 @@ cv2.moveWindow('image', 0,0)
 cv2.resizeWindow('image', windowWidth,windowHeight)
 
 def get_image_files():
+    """
+    Recursive directory lookup of image_path for *.jpg and *.png files
+    :return: a list of full-path image files
+    """
     image_files = []
     for dirpath, dirs, files in os.walk(image_path):
         for filename in files:
@@ -20,12 +30,16 @@ def get_image_files():
     return image_files
 
 def runSlideShow():
+    """
+    Main loop.  Open each image for wait_time mount of time until a key is pressed
+    :return: None
+    """
     while(True):
         for image in images:
             cv2.imshow('image', cv2.imread(image))
             if cv2.waitKey(wait_time) > 0:
                 cv2.destroyWindow('image')
-                return
+                return None
 
 
 if __name__ == '__main__':
